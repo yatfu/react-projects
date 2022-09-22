4292b5ddefa7cb5f0d07e8b9fa6e701b7502bb76
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-22 07:58:48 -1000

    added tours fetch

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,8 +4,40 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json();
+      setTours(tours)
+      //console.log(tours) // DEBUG
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+
+    console.log(tours)
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  },[])
+
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  }
+
+  else return <main>
+    <Tours />
+  </main>
 }
 
 export default App
