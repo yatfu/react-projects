e6fe1511b2ae88233c1373a2f821995c60290e05
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-22 07:43:12 -1000

    finished birthday project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setPeople] = useState(data);
+
+  return (
+  <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </section>
+  </main>
+  );
 }
+
+/** 
+ * {setPeople(temp.push(keanu))}
+ * 
+ * <button onClick={() => {
+  let temp = people;
+  const person = {
+    id: 6,
+    name: 'Keanu',
+    age: 24,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  };
+  setPeople(people.push(person))
+}}>Add Person</button> */
 
 export default App;
