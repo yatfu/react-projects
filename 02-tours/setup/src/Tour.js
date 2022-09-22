d4115a5e48f12aae272c1b248adb75d5d6cb75da
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-22 11:13:00 -1000

    added conditional rendering to info section of tours
    read more/read less

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
+  const [readMore, setReadMore] = useState(false) // used to toggle read more feature
   return <article className="single-tour">
     <img src={image} alt={name}/>
     <footer>
@@ -8,7 +9,9 @@ const Tour = ({id, image, info, price, name}) => {
         <h4>{name}</h4>
         <h4 className='tour-price'>${price}</h4>
       </div>
-      <p>{info}</p>
+      {/** If READMORE = true then display full info, else display substring */ }
+      <p>{readMore?info:`${info.substring(0,200)}...`}</p>
+      <button onClick={() => setReadMore(!readMore)}>{readMore?'Read Less' : 'Read More'}</button>
       <button className='delete-btn'>Not Interested</button>
     </footer>
   </article>
